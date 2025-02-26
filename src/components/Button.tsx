0f7b1234e8ac6a209c9bb0da937e577b8a388d4f
Naufal Zufar S <naufal.zufar.s@gmail.com>
2025-02-26 09:55:51 +0700

    feat: add button size

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,20 +2,29 @@ import { cn } from "@/lib/utils";
 import React from "react";
 
 const ButtonVariant = ["primary", "secondary", "outline"] as const;
+const ButtonSize = ["sm", "base"] as const;
 
 type ButtonProps = {
   variant?: (typeof ButtonVariant)[number];
+  size?: (typeof ButtonSize)[number];
 } & React.ComponentPropsWithRef<"button">;
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ children, className, disabled, variant = "primary" }, ref) => {
+  (
+    { children, className, disabled, variant = "primary", size = "base" },
+    ref
+  ) => {
     return (
       <button
         ref={ref}
         type="button"
         disabled={disabled}
         className={cn(
-          "font-semibold px-4 md:px-6 py-3 md:py-4 rounded-full transition-colors duration-75 focus:outline-none focus-visible:ring-amber-700 focus-visible:ring",
+          "font-semibold rounded-full transition-colors duration-75 focus:outline-none focus-visible:ring-amber-700 focus-visible:ring",
+          [
+            size === "base" && ["px-4 md:px-6 py-3 md:py-4"],
+            size === "sm" && ["px-4 py-2 text-sm"],
+          ],
           [
             variant === "primary" && [
               "text-white bg-amber-500 hover:bg-amber-400",
